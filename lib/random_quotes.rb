bb03701427653e722734a9d6eb5b4937b4b3ad94
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-03-30 01:37:55 +0530

    fix rubocop errors

--- lib/random_quotes.rb
+++ lib/random_quotes.rb
@@ -6,12 +6,12 @@ class RandomQuotes
   include HTTParty
 
   base_uri 'https://type.fit/api/quotes'
-  default_params fields: "smallIcon,shortDescription", q: "search"
-  
+  default_params fields: 'smallIcon,shortDescription', q: 'search'
+
   def self.quotes
-    response = get("https://type.fit/api/quotes")
+    response = get('https://type.fit/api/quotes')
     response = JSON.parse(response.body)
-    response[rand(500)]["text"]
+    response[rand(500)]['text']
   end
 end
 
