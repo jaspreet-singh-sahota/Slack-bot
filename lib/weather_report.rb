60f4915e124743991d669dd620839e607063eacc
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-03-30 03:57:03 +0530

    add weather_report.rb to lib folder

---
+++ lib/weather_report.rb
@@ -0,0 +1,24 @@
+require 'httparty'
+require 'pp'
+require 'json'
+
+class Weather
+  include HTTParty
+
+  base_uri 'https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139'
+  default_params fields: 'smallIcon,shortDescription', q: 'search'
+
+  def self.weather_report
+    response = get('https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139')
+    response = JSON.parse(response.body)
+    main = response['weather'][0]['main']
+    weather_icon = response['weather'][0]['icon']
+    temp = response['main']['temp']
+    pressure = response['main']['pressure']
+    humidity = response['main']['humidity']
+    temp_min = response['main']['temp_min']
+    temp_max = response['main']['temp_max']
+    wind_speed = response['wind']['speed']
+    report = "Weather: - #{main}\n Temperature: - #{temp}\n pressure: - #{pressure}\n Humidity: - #{humidity}\n Minimun Temprature:- #{temp_min}\n Maximun tempreatue: - #{temp_max}\n Wind Speed: - #{wind_speed}" 
+  end
+end
