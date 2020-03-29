a41b7d9a4a3c3a86ea82469126871a718b028314
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-03-30 01:34:19 +0530

    add class RandomQuotes

---
+++ lib/random_quotes.rb
@@ -0,0 +1,18 @@
+require 'httparty'
+require 'pp'
+require 'json'
+
+class RandomQuotes
+  include HTTParty
+
+  base_uri 'https://type.fit/api/quotes'
+  default_params fields: "smallIcon,shortDescription", q: "search"
+  
+  def self.quotes
+    response = get("https://type.fit/api/quotes")
+    response = JSON.parse(response.body)
+    response[rand(500)]["text"]
+  end
+end
+
+p RandomQuotes.quotes
