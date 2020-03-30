ca80e4c21ac59ae1931d863e243e72e4dfffba40
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-03-30 10:16:09 +0530

    add command covid_current_data

--- ruby-bot/bot.rb
+++ ruby-bot/bot.rb
@@ -2,7 +2,7 @@ module RubyBot
   class Bot < SlackRubyBot::Bot
     help do
       title 'slack Ruby Bot'
-      desc 'This bot is about Facts and Jokes.'
+      desc 'This bot do some pretty stuff.'
 
       command :say_hello do
         title 'say_hello'
@@ -17,6 +17,11 @@ module RubyBot
       command :weather_report do
       title 'weather_report'
       desc 'Return basic weather report'
+      end
+      
+      command :covid_current_data do
+      title 'covid_current_data'
+      desc 'Return the current data of the Covid-19'
       end
     end
   end
