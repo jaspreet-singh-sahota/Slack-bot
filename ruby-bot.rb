25b020063896f40690348342eab70c84b899c3f2
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-03-30 10:19:10 +0530

    add path to covid.rb

--- ruby-bot.rb
+++ ruby-bot.rb
@@ -1,5 +1,6 @@
 require 'slack-ruby-bot'
-require 'ruby-bot/commands/say_hello'
 require 'ruby-bot/bot'
+require 'ruby-bot/commands/say_hello'
 require 'ruby-bot/commands/weather_report'
 require 'ruby-bot/commands/random_quotes'
+require 'ruby-bot/commands/covid'
